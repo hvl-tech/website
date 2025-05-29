75dc016052df7868dc047f4c405b44bf65e3a7ab
danka <dana@hlavacova.de>
2025-05-29 16:32:28 +0200

    deploy

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -3,9 +3,6 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import i18next from "i18next";
 import de from "../translate/de.js";
 import en from "../translate/en.js";
-
-
-
 i18next
     .use(initReactI18next)
     .use(LanguageDetector)
