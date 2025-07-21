7076ba833832568f70d93859f49b54fd3744a35f
danka <dana@hlavacova.de>
2025-07-21 13:10:46 +0200

    preteir

--- src/component/aboutUs.tsx
+++ src/component/aboutUs.tsx
@@ -1,15 +1,15 @@
-import { Trans, useTranslation } from "react-i18next";
+import {Trans, useTranslation} from "react-i18next";
 
 export default function AboutUs() {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     return (
         <>
             <h2 className="font-['Press_Start_2P'] font-normal text-base text-[#00274a]">{t('aboutAs.highline')}</h2>
             <Trans
                 i18nKey='aboutAs.description'
                 components={[
-                    <p key="p" />,
-                    <i key="i" />
+                    <p key="p"/>,
+                    <i key="i"/>
                 ]}
             />
         </>
--- src/component/place.tsx
+++ src/component/place.tsx
@@ -1,8 +1,15 @@
 const Place = () => {
     return (
-        <div className='text-[#00274a] drop-shadow-[0_0_50px_black] text-center'>
-            <p> <span className='text-[#00274a] drop-shadow-[0_0_50px_white]'> Kulturhaus “Johannes R. Becher” </span> <br/> Havelländer Weg 67 <br/> Falkensee </p>
-        </div>
+        <section>
+            <div
+                className="bg-white gap-2.5 text-justify w-full px-8 py-8 flex flex-col items-center justify-around max-w-[1120px] mx-auto">
+                <div className='text-[#00274a] drop-shadow-[0_0_50px_black] text-center'>
+                    <p><span
+                        className='text-[#00274a] drop-shadow-[0_0_50px_white]'> Kulturhaus “Johannes R. Becher” </span>
+                        <br/> Havelländer Weg 67 <br/> Falkensee </p>
+                </div>
+            </div>
+        </section>
     );
 };
 
