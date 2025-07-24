0670ceb31313107e633f3f2b1492fa1a6d4c0e31
danka <dana@hlavacova.de>
2025-07-24 12:17:04 +0200

    email in food

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,7 +1,12 @@
+import text from '../assets/text.svg';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 export default function Footer() {
     return (
         <footer className="h-10 w-full bg-[#00274a]">
-
+<div className='flex justify-center items-center h-full w-full'>
+    <MailOutlineIcon style={{ color: 'white', height: '15px' }} />
+    <img src={text} alt='email' style={{ height: '15px' }} />
+</div>
         </footer>
     );
 }
