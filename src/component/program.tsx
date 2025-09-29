58a713589a7b96260045b21b0c6efa1255bc15c7
danka <dana@hlavacova.de>
2025-09-29 10:43:05 +0200

    img

--- src/component/program.tsx
+++ src/component/program.tsx
@@ -1,6 +1,6 @@
-import martin from '../assets/speakers/martinH.jpeg'
-import vladislav from '../assets/speakers/vladislavIavorsky.jpeg'
+import jonas from '../assets/speakers/jonas_benn.jpeg'
 import {useTranslation} from "react-i18next";
+
 
 
 type BlockProps = {
@@ -8,10 +8,11 @@ type BlockProps = {
     time: string;
 }
 type CardProps = {
-    photo: string;
+    photo?: string;
     startTime: string;
     name: string;
-    role: string;
+    role?: string;
+    heading: string;
     topic: string;
 };
 
@@ -23,19 +24,35 @@ const Block = ({nameBlock, time}: BlockProps) => {
     )
 }
 
-const Card = ({photo, startTime, name, role, topic}: CardProps) => {
+const Card = ({photo, startTime, name, role, heading, topic}: CardProps) => {
+
+    const initial = name.split(' ').map((name)=>name[0].toUpperCase())
 
     return (
         <div
             className=" border-4 border-black  shadow-[4px_4px_0px_#000] p-4 lg:w-2/3 w-full flex lg:flex-row items-center justify-between flex-col">
-            <div>
-                <img src={photo} alt={name}
-                     className="  md:w-40 md:h-auto  mb-4 border-4 border-black  shadow-[4px_4px_0px_#000] image-pixelated "/>
-            </div>
+
+            {photo ? ( <div>
+                        <img src={photo} alt={name}
+                                 className="w-auto h-auto md:w-40 md:h-auto border-4 border-black  shadow-[4px_4px_0px_#000] image-pixelated "/>
+              </div>
+                ):(
+                    <div
+                        className={`
+                                      md:w-40 md:h-40 w-full h-60  mb-4 md:mb-0 border-4 image-pixelated
+                                      text-white bg-green-700 border-black  shadow-[4px_4px_0px_#000]
+                                      lg:text-4xl md:text-3xl text-6xl 
+                                      flex items-center justify-center text-center whitespace-pre-line
+                                      font-['Press_Start_2P']
+                                    `}>
+                        {initial}
+                    </div>
+                ) }
+
             <div className='flex items-start flex-col lg:w-2/3 w-full'>
                 <h3 className="md:text-xl">{startTime}</h3>
-                <p className="font-bold mt-2">{name}</p>
-                <p className='text-xs'> {role}</p>
+                <p className="font-bold mt-2">{name} <span className='text-xs'> {role}</span></p>
+                <p className=" text-xl"> {heading} </p>
                 <p className="italic">{topic}</p>
             </div>
         </div>
@@ -54,14 +71,15 @@ const Program = () => {
 
                     <Block nameBlock={t('program.start')} time={'18:00'}/>
 
-                    <Card name={'Jonas Benn'} photo={martin} startTime={'18:30'} role={''}
-                          topic={'The Fediverse -- Beyond Centralized Social Networks\n' +
-                              'Description: A short introduction to the Fediverse and its underlying protocol ActivityPub'}/>
+                    <Card name={'Martin Pompéry'} startTime={'18:30'} heading={'Introduction to  Garble'}
+                          topic={'Garble is a simple programming language for Multi-Party Computation and is statically typed, low-level, purely functional and uses a Rust-like syntax.'}
+                    />
 
                     <Block nameBlock={t('program.disscusion')} time={'18:45-19:15'}/>
 
-                    <Card name={'Vladislav Iavorskii'} photo={vladislav} startTime={'19:15'}
-                          role={'Head of Engineering at Contorion'} topic={'EDI, Unboxed'}/>
+                    <Card name={'Jonas Benn'} photo={jonas} startTime={'19:15'}
+                          heading={'The Fediverse'} topic={'Beyond Centralized Social Networks\n' +
+                        'Description: A short introduction to the Fediverse and its underlying protocol ActivityPub'}/>
 
                     <Block nameBlock={t('program.disscusion')} time={'19:30-20:00'}/>
 
