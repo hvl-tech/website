a86d067dc5ad782021eab234c91f87699768b62b
Martin Hlavac <info@mhlavac.net>
2026-05-03 17:53:30 +0200

    Revert "Add Call for Speakers page and component (#36)"
    This reverts commit b79abea83c7ec4c6da440d0b8e1413afdc5069ba.

--- src/utils/buildMailto.ts
+++
@@ -1,5 +0,0 @@
-export function buildMailto(opts: { to: string; subject: string; body: string }): string {
-    const params = new URLSearchParams({ subject: opts.subject, body: opts.body });
-    const qs = params.toString().replace(/\+/g, '%20');
-    return `mailto:${opts.to}?${qs}`;
-}
